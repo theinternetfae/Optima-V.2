1eb3b502ec0cae9af64ebea195f81bdddfe5b3f7
Favour Egwele <favoregwele@gmail.com>
2025-11-19 15:12:34 +0100

    Revert "Making the days Array reusable with context"
    This reverts commit 537913895450c5c733f7d261d9bbaed82bb2cd42.

--- src/App.jsx
+++ src/App.jsx
@@ -7,40 +7,6 @@ import { TaskContext } from "./components/TaskContext.js";
 
 function App() {
 
-  const [days, setDays] = useState([]);
-
-    function getDaysAround(today, totalYears) {
-        
-        const result = [];
-        const start = new Date(today);
-        start.setFullYear(start.getFullYear() - 1);
-        const totalDays = totalYears * 365;
-
-        for (let i = 0; i < totalDays; i++) {
-            result.push(new Date(start));
-            start.setDate(start.getDate() + 1);
-        }
-
-        return result;
-        
-    }
-
-    // Fix to make sure that past dates show up as needed instead of only loading 1 year back in case user has data past that
-    // useEffect(() => {
-    //     const earliestTask = Math.min(...taskList.map(t => new Date(t.baseId)));
-    //     const latestTask = Math.max(...taskList.map(t => new Date(t.baseId)));
-
-    //     if (earliestTask < days[0].getTime() || latestTask > days[days.length - 1].getTime()) {
-    //         setDays(getDaysAround(today, 20));
-    //     }
-    // }, [taskList]);
-
-    useEffect(() => {
-        const today = new Date();
-        setDays(getDaysAround(today, 20));
-    }, []);
-  
-
   const [taskList, setTaskList] = useState(() => {
     const saved = localStorage.getItem("tasks");
     return saved ? JSON.parse(saved) : [];
@@ -61,7 +27,7 @@ function App() {
 
   return (
 
-    <TaskContext.Provider value={{taskList, setTaskList, tasksDone, setTasksDone, days, setDays}}>
+    <TaskContext.Provider value={{taskList, setTaskList, tasksDone, setTasksDone}}>
       <BrowserRouter>    
         <div className="named">
           <SideMenu/>
