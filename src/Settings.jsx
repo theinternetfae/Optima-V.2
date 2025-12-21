ca132baa105b8f65b38ef2596bc1b2ab47b509e9
Favour Egwele <favoregwele@gmail.com>
2025-12-21 11:24:56 +0100

    Setting up settings structure

--- src/Settings.jsx
+++ src/Settings.jsx
@@ -1,6 +1,26 @@
 function Settings() {
     return ( 
-        <>FINAL BOSS!</>
+        <div className="settings-main-cont">
+            
+            <h1 className="settings-header">Settings</h1>
+
+            <p className="settings-subheader">Manage your account settings and preferences</p>
+
+            <div className="settings-body">
+                <section className="settings-router">
+                    <ul>
+                        <li>Profile</li>
+                        <li>Preferences</li>
+                        <li>Task Handler</li>
+                        <li>Data & Privacy</li>
+                        <li>About</li>
+                    </ul>
+                </section>
+
+
+            </div>
+
+        </div>
     );
 }
 
