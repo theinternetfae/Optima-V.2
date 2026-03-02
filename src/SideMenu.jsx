a4abce6b1257ff0d11703d0c32f84ff723b258da
Favour Egwele <favoregwele@gmail.com>
2026-03-02 13:10:22 +0100

    Media query: side-menu

--- src/SideMenu.jsx
+++ src/SideMenu.jsx
@@ -15,12 +15,6 @@ function SideMenu() {
         return location.pathname;
     }, [location]);
 
-
-
-
-
-
-
     return ( 
         <nav className="side-menu">
 
@@ -28,22 +22,22 @@ function SideMenu() {
 
                 <Link to="/home" className={`road ${setPath === "/home" && 'border-[var(--color-accentprimary)]'}`}>
                     <i className="bi-house-fill"></i>
-                    <p>Home</p>
+                    <p className={`side-nav-hide ${setPath === "/home" && 'block'}`}>Home</p>
                 </Link>
 
                 <Link to="/taskStats" className={`road ${setPath === "/taskStats" && 'border-[var(--color-accentprimary)]'}`}>
                     <i className="bi-bar-chart-line"></i>
-                    <p>Task Stats</p>
+                    <p className={`side-nav-hide ${setPath === "/taskStats" && 'block'}`}>Stats</p>
                 </Link>
 
                 <Link to="/taskHistory" className={`road ${setPath === "/taskHistory" && 'border-[var(--color-accentprimary)]'}`}>
                     <i className="bi-clock-history"></i>
-                    <p>History</p>
+                    <p className={`side-nav-hide ${setPath === "/taskHistory" && 'block'}`}>History</p>
                 </Link>
 
                 <Link to="/settings" className={`road ${setPath === "/settings" && 'border-[var(--color-accentprimary)]'}`}>
                     <i className="bi bi-gear"></i>
-                    <p>Settings</p>
+                    <p className={`side-nav-hide ${setPath === "/settings" && 'block'}`}>Settings</p>
                 </Link>
 
             </div>
