627c77de75a9b5ed9e37c2bfdbc30c9bebcee492
Favour Egwele <favoregwele@gmail.com>
2026-03-10 15:23:34 +0100

    Filling up custom color picker

--- src/components/ColorPicker.jsx
+++ src/components/ColorPicker.jsx
@@ -1,28 +1,29 @@
 function ColorPicker({setColor, setCall}) {
     
     const customColors = [
-        "#FFFFFF",
-        "#FFFFFF",
-        "#FFFFFF",
-        "#FFFFFF",
-        "#FFFFFF",
-        "#FFFFFF",
-        "#FFFFFF",
-        "#FFFFFF",
-        "#FFFFFF",
-        "#FFFFFF",
-        "#FFFFFF",
-        "#FFFFFF",
-        "#FFFFFF",
-        "#FFFFFF",
-        "#FFFFFF",
-        "#FFFFFF",
-        "#FFFFFF",
-        "#FFFFFF",
-        "#FFFFFF",
-        "#FFFFFF"
+        "#EF4444",
+        "#DC2626",
+        "#F87171",
+        "#F97316",
+        "#FB923C",
+        "#EA580C",
+        "#FACC15",
+        "#EAB308",
+        "#F59E0B",
+        "#22C55E",
+        "#4ADE80",
+        "#16A34A",
+        "#14B8A6",
+        "#2DD4BF",
+        "#06B6D4",
+        "#3B82F6",
+        "#60A5FA",
+        "#2563EB",
+        "#8B5CF6",
+        "#A855F7",
+        "#EC4899"
     ]
-
+    
     return ( 
         <div className="color-picker">
             
